34d5698198cd2a566fe8097accc9863361dd322c
sy <sy@sydemacbook-pro.local>
2016-03-17 15:19:51 +0800

    创建了一个helloworld文件

---
+++ hello world/hello world/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello world
+//
+//  Created by sy on 16/3/17.
+//  Copyright © 2016年 sy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hello world/hello world/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  hello world
+//
+//  Created by sy on 16/3/17.
+//  Copyright © 2016年 sy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"HELLO WORLD!");
+    
+}
+
+@end
